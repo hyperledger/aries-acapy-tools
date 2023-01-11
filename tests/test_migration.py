e7672090a4f219101bcc8dbfc627e4833d31ed70
Char Howland <char@indicio.tech>
2023-01-11 10:57:14 -0700

    test: migration for sqlite database wallets
    Signed-off-by: Char Howland <char@indicio.tech>

---
+++ tests/test_migration.py
@@ -0,0 +1,30 @@
+import pytest
+import shutil
+import sys
+
+from acapy_wallet_upgrade.__main__ import upgrade
+from acapy_wallet_upgrade.sqlite_connection import SqliteConnection
+
+
+@pytest.mark.asyncio
+async def test_migration_sqlite(tmp_path):
+    """
+    Run the migration script with SQLite db files.
+    """
+    d = tmp_path / "sub"
+    d.mkdir()
+    key = "insecure"
+
+    # Alice
+    src_alice = f"{sys.path[0]}/tests/input/alice.db"
+    dst_alice = d / "alice.db"
+    shutil.copyfile(src_alice, dst_alice)
+    conn_alice = SqliteConnection(dst_alice)
+    await upgrade(conn_alice, key)
+
+    # Bob
+    src_bob = f"{sys.path[0]}/tests/input/bob.db"
+    dst_bob = d / "bob.db"
+    shutil.copyfile(src_bob, dst_bob)
+    conn_bob = SqliteConnection(dst_bob)
+    await upgrade(conn_bob, key)
