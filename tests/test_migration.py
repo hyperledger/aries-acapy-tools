01ef8511ffefbf216b70f74d31a68857ae2027f2
Char Howland <char@indicio.tech>
2023-01-11 11:25:20 -0700

    test: (WIP) dbpw migration
    Signed-off-by: Char Howland <char@indicio.tech>

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -2,6 +2,7 @@ import shutil
 from pathlib import Path
 
 import pytest
+import docker
 
 from acapy_wallet_upgrade.__main__ import upgrade
 from acapy_wallet_upgrade.sqlite_connection import SqliteConnection
@@ -29,3 +30,28 @@ async def test_migration_sqlite(tmp_path):
     shutil.copyfile(src_bob, dst_bob)
     conn_bob = SqliteConnection(dst_bob)
     await upgrade(conn_bob, key)
+
+
+def test_migration_dbpw(tmp_path):
+    """
+    Run the migration script with the db in the docker container.
+    """
+    d = tmp_path / "sub"
+    d.mkdir()
+    key = "insecure"
+
+    src = Path("input/dbpw")
+    dst = d / "dbpw"
+    shutil.copyfile(src, dst)
+
+    client = docker.from_env()
+
+    container_id = client.create_container(
+        'postgres:11', "localhost", volumes=[f'{dst}:/var/lib/postgresql/data'],
+        host_config=docker.utils.create_host_config(binds={
+            '/home/user1/': {
+                'bind': f'{dst}:/var/lib/postgresql/data',
+                'ro': True
+            }
+        })
+    )
