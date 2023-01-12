f921baee60d95077d76bf10e46955a2de5b52ba5
Char Howland <char@indicio.tech>
2023-01-12 12:37:58 -0700

    test: connect to postgres db in dbpw and mwst tests
    Signed-off-by: Char Howland <char@indicio.tech>

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,10 +1,12 @@
 import shutil
 from pathlib import Path
+import time
 
 import pytest
 import docker
 
 from acapy_wallet_upgrade.__main__ import upgrade
+from acapy_wallet_upgrade.pg_connection import PgConnection
 from acapy_wallet_upgrade.sqlite_connection import SqliteConnection
 
 
@@ -32,42 +34,18 @@ async def test_migration_sqlite(tmp_path):
     await upgrade(conn_bob, key)
 
 
-def test_migration_dbpw(tmp_path):
+@pytest.mark.asyncio
+async def test_migration_dbpw(tmp_path):
     """
     Run the migration script with the db in the docker container.
     """
+    src = Path("input/dbpw")
     d = tmp_path / "sub"
     d.mkdir()
-    key = "insecure"
-
-    src = Path("input/dbpw")
     dst = d / "dbpw"
     shutil.copytree(src, dst)
 
-    # docker stuff
     client = docker.from_env()
-    
-    try:
-        container = client.containers.get("indy-demo-postgres")
-    except docker.errors.NotFound:
-        pass
-    else:
-        container.stop()
-    try:
-        container = client.containers.run(
-            "postgres:11",
-            name="indy-demo-postgres",
-            volumes={dst: {"bind": "/var/lib/postgresql/data", "mode": "rw"}},
-            ports={"5432/tcp": 5432},
-            environment=["POSTGRES_PASSWORD=mysecretpassword"],
-            auto_remove=True,
-            detach=True,
-        )
-    except:
-        pass  # shh, Conceal it. Don't feel it. Don't let it show.
-    else:
-        container.stop()
-    dst = d / "mwst"
 
     try:
         container = client.containers.get("indy-demo-postgres")
@@ -85,7 +63,64 @@ def test_migration_dbpw(tmp_path):
             auto_remove=True,
             detach=True,
         )
+        time.sleep(4)
     except:
-        pass  # TODO: handle errors
+        pass  # shh, Conceal it. Don't feel it. Don't let it show.
+    # else:
+    #     container.stop()
+
+    conn = PgConnection(
+        db_host = "localhost",
+        db_name = "alice",
+        db_user = "postgres",
+        db_pass = "mysecretpassword",
+        path = "postgres://postgres:mysecretpassword@localhost:5432/alice"
+    )
+    key = "insecure"
+    await upgrade(conn, key)
+
+
+@pytest.mark.asyncio
+async def test_migration_mwst(tmp_path):
+    """
+    Run the migration script with the db in the docker container.
+    """
+    src = Path("input/mwst")
+    d = tmp_path / "sub"
+    d.mkdir()
+    dst = d / "mwst"
+    shutil.copytree(src, dst)
+
+    client = docker.from_env()
+
+    try:
+        container = client.containers.get("indy-demo-postgres")
+    except docker.errors.NotFound:
+        pass
     else:
         container.stop()
+    try:
+        container = client.containers.run(
+            "postgres:11",
+            name="indy-demo-postgres",
+            volumes={dst: {"bind": "/var/lib/postgresql/data", "mode": "rw"}},
+            ports={"5432/tcp": 5432},
+            environment=["POSTGRES_PASSWORD=mysecretpassword"],
+            auto_remove=True,
+            detach=True,
+        )
+        time.sleep(4)
+    except:
+        pass  # TODO: handle errors
+    # else:
+    #     container.stop()
+
+    conn = PgConnection(
+        db_host = "localhost",
+        db_name = "wallets",
+        db_user = "postgres",
+        db_pass= "mysecretpassword",
+        path = "postgres://postgres:mysecretpassword@localhosts:5432/wallets"
+    )
+    key = "insecure"
+    await upgrade(conn, key)
