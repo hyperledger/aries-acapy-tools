f0c8ca080304487e894109a215640cafd88ef388
Adam Burdett <burdettadam@gmail.com>
2023-02-02 12:23:52 -0700

    migrate in the middle
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- tests/e2e/cases.py
+++ tests/e2e/cases.py
@@ -1,16 +1,15 @@
 import time
 from typing import Any, AsyncGenerator, Dict, List, Mapping, Tuple
+
 from controller import Controller
-from controller.protocols import (
-    didexchange,
-    indy_anoncred_credential_artifacts,
-    indy_anoncred_onboard,
-    indy_issue_credential_v1,
-    indy_issue_credential_v2,
-    indy_present_proof_v1,
-    indy_present_proof_v2,
-    indy_anoncreds_revoke,
-)
+from controller.protocols import (CredAttrSpec, CredentialPreview,
+                                  V10CredentialConnFreeOfferRequest,
+                                  V10CredentialExchange, didexchange,
+                                  indy_anoncred_credential_artifacts,
+                                  indy_anoncred_onboard, indy_anoncreds_revoke,
+                                  indy_issue_credential_v1,
+                                  indy_issue_credential_v2,
+                                  indy_present_proof_v1, indy_present_proof_v2)
 
 
 class MigrationTestCases:
@@ -21,10 +20,10 @@ class MigrationTestCases:
             self.credentials_without_revocation_v2(),
             self.credentials_with_revocation(),
             self.revoked_credential(
-                {"first_name": "Bob", "last_name": "Builder"},
+                {"revoked_test": "Bob", "revoked_last": "Builder"},
                 [
                     {
-                        "name": "first_name",
+                        "name": "revoked_test",
                     }
                 ],
                 supports_revocation=True,
@@ -42,6 +41,17 @@ class MigrationTestCases:
                     },
                 ],
                 supports_revocation=False,
+            ),
+            self.migration_during_credential(
+                {"abc": "Bob", "defg": "Builder"},
+                [
+                    {
+                        "name": "abc",
+                    },
+                    {
+                        "name": "defg",
+                    },
+                ],
             ),
         )
 
@@ -162,10 +172,10 @@ class MigrationTestCases:
     ) -> AsyncGenerator[None, Tuple[Controller, Controller]]:
         now = int(time.time())
         return self.credentials_v1(
-            {"first_name": "Bob", "last_name": "Builder"},
+            {"first_name0": "Bob", "last_name": "Builder"},
             [
                 {
-                    "name": "first_name",
+                    "name": "first_name0",
                 }
             ],
             supports_revocation=True,
@@ -173,15 +183,15 @@ class MigrationTestCases:
 
     def credentials_without_revocation(self):
         return self.credentials_v1(
-            {"first_name": "Alice", "last_name": "Cooper"},
-            [{"name": "first_name"}],
+            {"first_name1": "Alice", "last_name": "Cooper"},
+            [{"name": "first_name1"}],
             supports_revocation=False,
         )
 
     def credentials_without_revocation_v2(self):
         return self.credentials_v2(
-            {"first_name": "Alice", "last_name": "Cooper"},
-            [{"name": "first_name"}],
+            {"first_name2": "Alice", "last_name": "Cooper"},
+            [{"name": "first_name2"}],
             supports_revocation=False,
         )
 
@@ -288,6 +298,105 @@ class MigrationTestCases:
             alice,
             bob_conn.connection_id,
             alice_conn.connection_id,
+            requested_attributes=requested_attributes,
+        )
+        assert alice_pres_ex_askar.state == "verified"
+        assert alice_pres_ex_askar.verified == "true"
+
+    async def migration_during_credential(
+        self, cred_attrs, requested_attributes
+    ) -> AsyncGenerator[None, Tuple[Controller, Controller]]:
+        issuer, holder = yield
+
+        issuer_conn, holder_conn = await didexchange(issuer, holder)
+        # Issuance prep
+        await indy_anoncred_onboard(issuer)
+        schema, cred_def = await indy_anoncred_credential_artifacts(
+            issuer,
+            list(cred_attrs.keys()),
+            support_revocation=False,
+        )
+        # Issue the thing
+        issuer_cred_ex = await issuer.post(
+            "/issue-credential/send-offer",
+            json=V10CredentialConnFreeOfferRequest(
+                auto_issue=False,
+                auto_remove=False,
+                comment="Credential from minimal example",
+                trace=False,
+                connection_id=issuer_conn.connection_id,
+                cred_def_id=cred_def.credential_definition_id,
+                credential_preview=CredentialPreview(
+                    type="issue-credential/1.0/credential-preview",  # pyright: ignore
+                    attributes=[
+                        CredAttrSpec(
+                            mime_type=None, name=name, value=value  # pyright: ignore
+                        )
+                        for name, value in cred_attrs.items()
+                    ],
+                ),
+            ),
+            response=V10CredentialExchange,
+        )
+        issuer_cred_ex_id = issuer_cred_ex.credential_exchange_id
+
+        holder_cred_ex = await holder.record_with_values(
+            topic="issue_credential",
+            record_type=V10CredentialExchange,
+            connection_id=holder_conn.connection_id,
+            state="offer_received",
+        )
+        holder_cred_ex_id = holder_cred_ex.credential_exchange_id
+
+        holder_cred_ex = await holder.post(
+            f"/issue-credential/records/{holder_cred_ex_id}/send-request",
+            response=V10CredentialExchange,
+        )
+
+        await issuer.record_with_values(
+            topic="issue_credential",
+            credential_exchange_id=issuer_cred_ex_id,
+            state="request_received",
+        )
+
+        issuer, holder = yield  # migrate ....
+
+        issuer_cred_ex = await issuer.post(
+            f"/issue-credential/records/{issuer_cred_ex_id}/issue",
+            json={},
+            response=V10CredentialExchange,
+        )
+
+        await holder.record_with_values(
+            topic="issue_credential",
+            credential_exchange_id=holder_cred_ex_id,
+            state="credential_received",
+        )
+
+        holder_cred_ex = await holder.post(
+            f"/issue-credential/records/{holder_cred_ex_id}/store",
+            json={},
+            response=V10CredentialExchange,
+        )
+        issuer_cred_ex = await issuer.record_with_values(
+            topic="issue_credential",
+            record_type=V10CredentialExchange,
+            credential_exchange_id=issuer_cred_ex_id,
+            state="credential_acked",
+        )
+
+        holder_cred_ex = await holder.record_with_values(
+            topic="issue_credential",
+            record_type=V10CredentialExchange,
+            credential_exchange_id=holder_cred_ex_id,
+            state="credential_acked",
+        )
+
+        _, alice_pres_ex_askar = await indy_present_proof_v1(
+            holder,
+            issuer,
+            holder_conn.connection_id,
+            issuer_conn.connection_id,
             requested_attributes=requested_attributes,
         )
         assert alice_pres_ex_askar.state == "verified"
